7010d4119c1a7e94776ea3ebd77c26a4f7a85d88
Hoang Vo~ <vohoang2204@gmail.com>
2025-04-12 11:17:03 +0700

    file

---
+++ HuggingFace/bpe_tok.py
@@ -0,0 +1,26 @@
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
+import pandas as pd
+import pyarrow
+
+
+def read_dataset(split="train"):
+    return pd.read_parquet(fr"D:\Programming\Python\Tokenization\Data\{split}-00000-of-00001.parquet")
+
+
+if __name__ == "__main__":
+    # Define special tokens
+    print(1)
+    unk_token = "[UNK]"
+    pad_token = "[PAD]"
+    mask_token = "[MASK]"
+    sos_token = "<sos>"
+    eos_token = "<eos>"
+    special_tokens = [unk_token, pad_token, sos_token, eos_token]
+
+    en_tokenizer = Tokenizer(BPE(unk_token=unk_token))
+    en_tokenizer.pre_tokenizer = Whitespace()
+    en_trainer = BpeTrainer(special_tokens=special_tokens)
+    en_tokenizer.train_from_iterator(read_dataset('train')['en'].tolist(), trainer=en_trainer)
